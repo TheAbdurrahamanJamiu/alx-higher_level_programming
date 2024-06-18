676d2b7dd3bc04fbd0633b1d1cd959f9d2351736
TheAbdurrahamanJamiu <111193782+TheAbdurrahamanJamiu@users.noreply.github.com>
2024-06-18 13:49:13 +0100

    Delete 0x12-javascript-warm_up directory

--- 0x12-javascript-warm_up/4-concat.js
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/node
-conslole.log(process.argv[2] + ' is ' + process.argv[3]);
--- 0x12-javascript-warm_up/9-add.js
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/node
-fuction add (a, b) {
-	const c = a + b;
-	console.log(c);
-}
-add(Number(process.argv[2]), Number(process.argv[3]));
