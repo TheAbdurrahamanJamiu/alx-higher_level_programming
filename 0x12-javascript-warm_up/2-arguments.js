f9c53129e85b5f6772d326f1648769d5d198829d
TheAbdurrahamanJamiu <abdurrahamanjamiu75@outlook.com>
2024-08-01 03:26:19 -0700

    here we go

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -3,9 +3,9 @@
 const args = process.argv;
 
 if (args.length <= 2) {
-	console.log('No argument');
+  console.log('No argument');
 } else if (args.length === 3) {
-	console.log('Arguments found');
+  console.log('Arguments found');
 } else {
-	console.log('arguments found');
+  console.log('arguments found');
 }
